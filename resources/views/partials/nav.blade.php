0d03ae3cf5ee58fef472d3dd8bd1a2c7f911a5d8
Boximayo <mayo_black@outlook.com>
2022-01-26 16:09:41 -0700

    Nav produccion

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -34,18 +34,18 @@
 
         <ul class="mx nav nav-fill" style="background-color: #dbad3a !important">
             <li class="nav-item {{ setActive('inicio.*')}}">
-                <a class="nav-link active " href="http://tsjescuelajudicialnay.herokuapp.com/inicio"><svg class="mr-1 " style="margin-top: -5px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4c0d0d" class="bi bi-house  mt-2" viewBox="0 0 16 16">
+                <a class="nav-link active " href="{{ route('inicio.index')}}"><svg class="mr-1 " style="margin-top: -5px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4c0d0d" class="bi bi-house  mt-2" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                     <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
                 </svg>Inicio</a>
             </li>
             <li class="nav-item {{ setActive('curso.index')}}">
-                <a class="nav-link " href="http://tsjescuelajudicialnay.herokuapp.com/curso"><svg class="mr-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4c0d0d" class="bi bi-book mt-2  " viewBox="0 0 16 16">
+                <a class="nav-link " href="{{ route('curso.index')}}"><svg class="mr-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4c0d0d" class="bi bi-book mt-2  " viewBox="0 0 16 16">
                     <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
                   </svg>Cursos y Diplomados</a>
             </li>
             <li class="nav-item  {{ setActive('perfil.*')}}">
-                <a class="nav-link" href="http://tsjescuelajudicialnay.herokuapp.com/perfil"><svg class="mr-1" style="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4c0d0d" class="bi bi-person " viewBox="0 0 16 16">
+                <a class="nav-link" href="{{ route('perfil.index')}}"><svg class="mr-1" style="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4c0d0d" class="bi bi-person " viewBox="0 0 16 16">
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                   </svg>Mi Perfil</a>
             </li>
